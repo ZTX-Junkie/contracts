8091eb823ef2e282d15735dd865629f73795b328
ZTX-Junkie <66398234+ZTX-Junkie@users.noreply.github.com>
2021-12-25 16:53:21 +0100

    Update Dredcore-token.sol

--- Dredcore-token.sol
+++ Dredcore-token.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.6.8;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DredCore is ERC20 {
-    constructor() ERC20("Dred Core ", "DRD") {
+contract DredCorev2 is ERC20 {
+    constructor() ERC20("Dred Corev2 ", "DRDCORE") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
